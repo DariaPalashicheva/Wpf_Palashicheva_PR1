d4e5d9edc0413433fda6b7132f71855fe08b0e86
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-09-22 16:58:35 +1000

    Кнопка нажать

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Wpf_Palashicheva_PR1
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Привет, группа 402 ИСП!");
+        }
     }
 }
