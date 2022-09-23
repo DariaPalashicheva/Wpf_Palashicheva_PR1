01404809fe4d772abb3ba26c456a60df242a1e16
palashichevaa@mail.ru <palashichevaa@mail.ru>
2022-09-23 15:43:55 +1000

    Win6 Grid и кнопка

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -54,5 +54,11 @@ namespace Wpf_Palashicheva_PR1
             Window5 window5 = new Window5();
             window5.Show();
         }
+
+        private void Win6_Click(object sender, RoutedEventArgs e)
+        {
+            Window6 window6 = new Window6();
+            window6.Show();
+        }
     }
 }
